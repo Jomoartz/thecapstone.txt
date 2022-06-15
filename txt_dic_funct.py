36af9dafcbd3599a022bc0439e96452fc49cbee0
Thecapstone <105176170+Thecapstone@users.noreply.github.com>
2022-06-15 14:01:26 +0100

    Update txt_dic_funct.py

--- txt_dic_funct.py
+++ txt_dic_funct.py
@@ -1,18 +1,82 @@
-with open("story.txt" ) as b:
-	content= b.readlines()
-	contentz=content.copy()
-	b.close()
+def read_file_content(story_txt):
+	with open("story.txt" ) as b:
+		content= b.readlines()
+		contentz=content.copy()
+		return(contentz)
+		b.close()
 
-pringles=(' '. join(contentz))
-bam= pringles.split(' ')
 
-
-story={
-	"stress": 1,
-	}
 	
-	words= bam
+def count_words(contentz):
+	get=read_file_content(contentz)
+	text=(' '. join(get))
+	split = text.split(' ')
+		
+		
+	story={
+	"professor": 1,
+	}
+			
+	words= split
 	for items in words:
 		count= words.count(items)
 		story[items]= count
-	print(story)
+		print(story)
+	
+
+print(count_words("story.txt"))
+	
+		
+		
+		
+		
+
+	
+		
+	
+	
+	
+		
+		
+	
+	
+	
+			
+	
+	
+		
+		
+		
+
+	
+	
+		
+		
+	
+	
+	
+			
+	
+	
+	
+		
+				
+		
+	
+	
+	
+	
+
+
+
+
+
+
+
+	
+	
+
+
+	
+		
+	
